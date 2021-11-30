29cbfc74fbd1134ebc692ca56b10331224b86b0a
teeraphat147258 <teeraphat147258@gmail.com>
2021-11-30 16:53:54 +0700

    lab7_2 completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,29 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
+
+int adiff(int a,int b){
+int c = abs((a%360)-(b%360));
+
+   if(c>180){
+    int x = 360-c;
+    return x;
+    }
+    else{
+     return c;
+  }
+}
+
+
 
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << "\n";
+  cout << adiff(210,45) << "\n";
+  cout << adiff(0,360) << "\n";
+  cout << adiff(10,350) << "\n";
+  cout << adiff(95,260) << "\n";
+  cout << adiff(90,-90) << "\n";
+  cout << adiff(1000,280) << "\n";
+  cout << adiff(60,244) << "\n";
 }
